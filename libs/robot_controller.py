dd3aa891e8ede3c2f5f029bb7f18131d8c30ab58
mehrinka <mehrinka@rose-hulman.edu>
2018-02-06 12:28:16 -0500

    Kyle digital_inputs

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -97,3 +97,12 @@ class Snatch3r(object):
         ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
         print("Goodbye")
         ev3.Sound.speak("Goodbye")
+
+    def loop_forever(self):
+        # This is a convenience method that I don't really recommend for most programs other than m5.
+        #   This method is only useful if the only input to the robot is coming via mqtt.
+        #   MQTT messages will still call methods, but no other input or output happens.
+        # This method is given here since the concept might be confusing.
+        self.running = True
+        while self.running:
+            time.sleep(0.1)  # Do nothing (except receive MQTT messages) until an MQTT message calls shutdown.
