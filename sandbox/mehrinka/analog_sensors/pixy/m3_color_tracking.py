5bb14560588d702b332d0724f222488b301c5af6
mehrinka <mehrinka@rose-hulman.edu>
2018-02-08 09:31:30 -0500

    Kyle analog sensors

--- sandbox/mehrinka/analog_sensors/pixy/m3_color_tracking.py
+++ sandbox/mehrinka/analog_sensors/pixy/m3_color_tracking.py
@@ -5,8 +5,8 @@ the robot so that it is always facing the color signature.  You will need to tea
 implement the code, then make the robot always face the color as you move it around.  The robot will only spin and never
 move forwards or backwards.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Kyle Mehringer.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -29,23 +29,36 @@ def main():
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 2. Read the Pixy values for x and y
+        # Done: 2. Read the Pixy values for x and y
         # Print the values for x and y
 
-        # TODO: 3. Use the x value to turn the robot
+        x = robot.pixy.value(1)
+        y = robot.pixy.value(2)
+        print('X Value:', x)
+        print('Y Value:', y)
+
+        # Done: 3. Use the x value to turn the robot
         #   If the Pixy x value is less than 150 turn left (-turn_speed, turn_speed)
         #   If the Pixy x value is greater than 170 turn right (turn_speed, -turn_speed)
         #   If the Pixy x value is between 150 and 170 stop the robot
         # Continuously track the color until the touch sensor is pressed to end the program.
 
-
+        if x < 150:
+            robot.left_motor.run_forever(speed_sp=-turn_speed)
+            robot.right_motor.run_forever(speed_sp=turn_speed)
+        elif x > 170:
+            robot.left_motor.run_forever(speed_sp=turn_speed)
+            robot.right_motor.run_forever(speed_sp=-turn_speed)
+        else:
+            robot.left_motor.run_forever(speed_sp=0)
+            robot.right_motor.run_forever(speed_sp=0)
 
         time.sleep(0.25)
 
     print("Goodbye!")
     ev3.Sound.speak("Goodbye").wait()
 
-# TODO: 4. Call over a TA or instructor to sign your team's checkoff sheet.
+# Done: 4. Call over a TA or instructor to sign your team's checkoff sheet.
 #
 # Observations you should make, the Pixy cam could potentially be used for a lot of cool project ideas, but if you
 # decide to use the Pixy you should be aware of what it does well and what it doesn't do well.
