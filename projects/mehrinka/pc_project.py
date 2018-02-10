f07f1097d64390a85de466eb183da835c05131c9
mehrinka <mehrinka@rose-hulman.edu>
2018-02-10 11:31:19 -0500

    Kyle's Personal Project

--- projects/mehrinka/pc_project.py
+++ projects/mehrinka/pc_project.py
@@ -1,29 +1,6 @@
-#!/usr/bin/env python3
-"""
-This module is the mini-project for the MQTT unit.  This module will be running on your PC and communicating with the
-m5_ev3_remote_drive.py module that is running on your EV3 (you have to write that module too, but it's easier).
-Only the Tkinter GUI has been made for you.  You will need to implement all of the MQTT communication.  The goal is to
-have a program running on your computer that can control the EV3.
-
-You will need to have the following features:
-  -- Clickable drive direction buttons to drive forward (up), backwards (down), left, right, and stop (space)
-    -- Keyboard shortcut keys that behave the same as clicking the buttons (this has already been wired up for you)
-  -- An entry box for the left and right drive motor speeds.
-    -- If both become set to 900 all of the drive direction buttons will go fast, for example forward goes 900 900
-    -- If both become set to 300 all of the drive direction buttons will go slower, for example reverse goes -300 -300
-    -- If 500 then left does -500 500, which causes the robot to spin left (use half speed -250 250 if too fast)
-    -- If set differently to say 600 left, 300 right the robot will drive and arc, for example forward goes 600 300
-  -- In addition to the drive features there needs to be a clickable button for Arm Up and Arm Down
-    -- There also need to be keyboard shortcut for Arm Up (u) and Arm Down (j).  Arm calibration is not required.
-
-  -- Finally you need 2 buttons for ending your program:
-    -- Quit, which stops only this program and allows the EV3 program to keep running
-    -- Exit, which sends a shutdown message to the EV3, then ends it's own program as well.
-
-You can start by running the code to see the GUI, but don't expect button clicks to do anything useful yet.
-
-Authors: David Fisher and Kyle Mehringer.
-"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""The goal for this project is to create a program that will allow a robot to travel along a path and collect data
+images along the way, representing a tourist taking pictures. This code is written by Kyle Mehringer for the final
+project in CSSE120 due on February 20, 2018. Some code was taken from previous modules written by Dave Fisher."""
 
 import tkinter
 from tkinter import ttk
@@ -32,8 +9,6 @@ import mqtt_remote_method_calls as com
 
 
 def main():
-    # Done: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
-    # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
 
     mqtt_client = com.MqttClient()
     mqtt_client.connect_to_ev3()
@@ -56,15 +31,9 @@ def main():
     right_speed_entry.insert(0, "600")
     right_speed_entry.grid(row=1, column=2)
 
-    # Done: 3. Implement the callbacks for the drive buttons. Set both the click and shortcut key callbacks.
-    #
-    # To help get you started the arm up and down buttons have been implemented.
-    # You need to implement the five drive buttons.  One has been writen below to help get you started but is commented
-    # out. You will need to change some_callback1 to some better name, then pattern match for other button / key combos.
-
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
-    # forward_button and '<Up>' key is done for your here...
+    # forward_button and '<Up>' key
     forward_button['command'] = lambda: drive_forward(mqtt_client, left_speed_entry, right_speed_entry)
     root.bind('<Up>', lambda event: drive_forward(mqtt_client, left_speed_entry, right_speed_entry))
 
@@ -117,7 +86,6 @@ def main():
 # ----------------------------------------------------------------------
 # Tkinter callbacks
 # ----------------------------------------------------------------------
-# Done: 4. Implement the functions for the drive button callbacks.
 
 def drive_forward(mqtt_client, left_speed_entry, right_speed_entry):
     print("drive_forward")
@@ -142,12 +110,6 @@ def turn_right(mqtt_client, left_speed_entry, right_speed_entry):
 def drive_back(mqtt_client, left_speed_entry, right_speed_entry):
     print("drive_backward")
     mqtt_client.send_message("drive_backward", [int(left_speed_entry.get()), int(right_speed_entry.get())])
-
-
-# Done: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
-#
-# Observations you should make, you did basically this same program using the IR Remote, but your computer can be a
-# remote control that can do A LOT more than an IR Remote.  We are just doing the basics here.
 
 
 # Arm command callbacks
