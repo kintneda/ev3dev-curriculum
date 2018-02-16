e3b7a06abe1e6d9561271dadef089c4833c1f0df
TDNasty21 <32580253+TDNasty21@users.noreply.github.com>
2018-02-16 14:35:02 -0500

    Pc

--- projects/nottintd/ev3_project_pc.py
+++ projects/nottintd/ev3_project_pc.py
@@ -7,7 +7,6 @@ import mqtt_remote_method_calls as com
 import tkinter
 from tkinter import ttk
 
-class MyDelegate(object):
 
 def main():
     mqtt = com.MqttClient()
@@ -25,24 +24,24 @@ def main():
     burger_label = ttk.Label(main_frame, text="Burgers", font=("Helvetica", 16))
     burger_label.grid(row=1, columnspan=2)
 
-    quarter_pounder_checkbutton = ttk.Checkbutton(main_frame, text="1/4 Pounder", offvalue=None,
-                                                  onvalue="quarter pounder")
+    quarter_pounder_checkbutton = ttk.Checkbutton(main_frame, text="1/4 Pounder", onvalue="quarter pounder",
+                                                  offvalue="")
     quarter_pounder_checkbutton.grid(row=2, column=0, sticky="W")
     quarter_pounder_value = tkinter.StringVar()
     quarter_pounder_checkbutton['variable'] = quarter_pounder_value
 
-    half_pounder_checkbutton = ttk.Checkbutton(main_frame, text="1/2 Pounder", onvalue="half pounder")
+    half_pounder_checkbutton = ttk.Checkbutton(main_frame, text="1/2 Pounder", onvalue="half pounder", offvalue="")
     half_pounder_checkbutton.grid(row=2, column=1, sticky="W")
     half_pounder_value = tkinter.StringVar()
     half_pounder_checkbutton['variable'] = half_pounder_value
 
-    cheeseburger_checkbutton = ttk.Checkbutton(main_frame, text="Cheeseburger", onvalue="cheeseburger")
+    cheeseburger_checkbutton = ttk.Checkbutton(main_frame, text="Cheeseburger", onvalue="cheeseburger", offvalue="")
     cheeseburger_checkbutton.grid(row=3, column=0, sticky="W")
     cheeseburger_value = tkinter.StringVar()
     cheeseburger_checkbutton['variable'] = cheeseburger_value
 
     double_cheeseburger_checkbutton = ttk.Checkbutton(main_frame, text="Double Cheeseburger",
-                                                      onvalue="double cheeseburger")
+                                                      onvalue="double cheeseburger", offvalue="")
     double_cheeseburger_checkbutton.grid(row=3, column=1, sticky="W")
     double_cheeseburger_value = tkinter.StringVar()
     double_cheeseburger_checkbutton['variable'] = double_cheeseburger_value
@@ -50,17 +49,17 @@ def main():
     fries_label = ttk.Label(main_frame, text="Fries", font=("Helvetica", 16))
     fries_label.grid(row=4, columnspan=2)
 
-    small_fries_checkbutton = ttk.Checkbutton(main_frame, text="Small", onvalue="small fries")
+    small_fries_checkbutton = ttk.Checkbutton(main_frame, text="Small", onvalue="small fries", offvalue="")
     small_fries_checkbutton.grid(row=5, column=0, sticky="W")
     small_fries_value = tkinter.StringVar()
     small_fries_checkbutton['variable'] = small_fries_value
 
-    medium_fries_checkbutton = ttk.Checkbutton(main_frame, text="Medium", onvalue="medium fries")
+    medium_fries_checkbutton = ttk.Checkbutton(main_frame, text="Medium", onvalue="medium fries", offvalue="")
     medium_fries_checkbutton.grid(row=5, column=1, sticky="W")
     medium_fries_value = tkinter.StringVar()
     medium_fries_checkbutton['variable'] = medium_fries_value
 
-    large_fries_checkbutton = ttk.Checkbutton(main_frame, text="Large", onvalue="large fries")
+    large_fries_checkbutton = ttk.Checkbutton(main_frame, text="Large", onvalue="large fries", offvalue="")
     large_fries_checkbutton.grid(row=6, column=0, sticky="W")
     large_fries_value = tkinter.StringVar()
     large_fries_checkbutton['variable'] = large_fries_value
@@ -68,22 +67,22 @@ def main():
     drinks_label = ttk.Label(main_frame, text="Drinks", font=("Helvetica", 16))
     drinks_label.grid(row=7, columnspan=4)
 
-    water_checkbutton = ttk.Checkbutton(main_frame, text="Water", onvalue="water")
+    water_checkbutton = ttk.Checkbutton(main_frame, text="Water", onvalue="water", offvalue="")
     water_checkbutton.grid(row=8, column=0, sticky="W")
     water_value = tkinter.StringVar()
     water_checkbutton['variable'] = water_value
 
-    soda_checkbutton = ttk.Checkbutton(main_frame, text="Soda", onvalue="Soda")
+    soda_checkbutton = ttk.Checkbutton(main_frame, text="Soda", onvalue="Soda", offvalue="")
     soda_checkbutton.grid(row=8, column=1, sticky="W")
     soda_value = tkinter.StringVar()
     soda_checkbutton['variable'] = soda_value
 
-    juice_checkbutton = ttk.Checkbutton(main_frame, text="Juice", onvalue="juice")
+    juice_checkbutton = ttk.Checkbutton(main_frame, text="Juice", onvalue="juice", offvalue="")
     juice_checkbutton.grid(row=9, column=0, sticky="W")
     juice_value = tkinter.StringVar()
     juice_checkbutton['variable'] = juice_value
 
-    coffee_checkbutton = ttk.Checkbutton(main_frame, text="Coffee", onvalue="coffee")
+    coffee_checkbutton = ttk.Checkbutton(main_frame, text="Coffee", onvalue="coffee", offvalue="")
     coffee_checkbutton.grid(row=9, column=1, sticky="W")
     coffee_value = tkinter.StringVar()
     coffee_checkbutton['variable'] = coffee_value
@@ -112,17 +111,70 @@ def get_order(quarter_pounder_value, half_pounder_value, cheeseburger_value, dou
             your_order = your_order + [rough_order[k]]
     print(your_order)
 
-    ev3.Sound.speak("Your order is a").wait()
-    for k in range(len(your_order)):
-        ev3.Sound.speak(your_order[k]).wait()
+    # ev3.Sound.speak("Your order is a").wait()
+    # for k in range(len(your_order)):
+    #     ev3.Sound.speak(your_order[k]).wait()
 
     for k in range(len(your_order)):
-        if your_order[k] == "quarter pounder" or "half pounder" or "cheeseburger" or "double cheeseburger":
-            mqtt.send_message("make_burger")
-        if your_order[k] == "small fries" or "medium fries" or "large fries":
-            mqtt.send_message("get_fries")
-        if your_order[k] == "water" or "soda" or "juice" or "coffee":
-            mqtt.send_message("get_drink", )
+        if your_order[k] == str('quarter pounder'):
+            print("Making your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('half pounder'):
+            print("Making your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('cheeseburger'):
+            print("Making your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('double cheeseburger'):
+            print("Making your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('small fries'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('medium fries'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('large fries'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('water'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('soda'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('juice'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+        if your_order[k] == str('coffee'):
+            print("Getting your ", end='')
+            print(your_order[k])
+            # mqtt.send_message("make_burger")
+
+
+
+
+
 
 
 main()
